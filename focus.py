594a3be2bfdc1795ef0dea1f0bfbc1d3d374d187
Andrew Moffat <andrew.robert.moffat@gmail.com>
2012-05-29 21:22:48 -0500

    removing some repetition

--- focus.py
+++ focus.py
@@ -374,8 +374,8 @@ if __name__ == "__main__":
                 resolv_conf)
             while not nameservers:
                 nameservers = load_nameservers(resolv_conf)
-                if config["bind_ip"] in nameservers:
-                    nameservers.remove(config["bind_ip"])
+                try: nameservers.remove(config["bind_ip"])
+                except ValueError: pass
                 time.sleep(5)
 
             log.info("found an alternative nameserver")
